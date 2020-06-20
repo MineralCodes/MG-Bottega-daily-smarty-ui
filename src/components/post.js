88d2478ea4af1783a793d8eabf4dbe8b570ecaa6
Micah Gilbertson <micah.gilbertson@gmail.com>
2020-06-19 21:24:26 -0500

    recommit of previous commit

--- src/components/post.js
+++ src/components/post.js
@@ -13,6 +13,21 @@ export default class Post extends Component {
 		return topics;
 	}
 
+	renderUsefulLinks() {
+		let links = this.props.post_links.map((post_link, index) => {
+			return (
+				<div key={index} className="useful-link">
+					<div className="post-link-box"></div>
+					<div className="post-link-link">
+						<a href={post_link.link_url}>Useful Link #{index + 1}</a>
+					</div>
+				</div>
+			);
+		});
+
+		return links;
+	}
+
 	render() {
 		if (this.props.type === "recent") {
 			return (
@@ -23,9 +38,10 @@ export default class Post extends Component {
 			);
 		} else if (this.props.type === "result") {
 			return (
-				<li className="recent-post">
-					<div className="recent-post-title">{this.props.title}</div>
-					<div className="recent-post-topics">{this.renderTopics()}</div>
+				<li className="result-post">
+					<div className="result-post-topics">{this.renderTopics()}</div>
+					<div className="result-post-title">{this.props.title}</div>
+					<div className="result-post-links">{this.renderUsefulLinks()}</div>
 				</li>
 			);
 		}
